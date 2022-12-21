665bde74d20bb3bc40c5bdd2bf3dfd542d4f0ce1
Daniel <daniel@Zipcoders-MacBook-Pro-10.local>
2022-12-21 16:03:51 -0500

    passing calc tests except array of numbers

--- src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
@@ -1,35 +1,54 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class Calculator {
     public static Double squareRoot(Double value) {
-        return null;
+
+        return Math.sqrt(value);
     }
 
     public static Double square(Double value) {
-        return null;
+        return value * value;
     }
 
     public static Double[] squareRoots(Double... value) {
+//        for(value : squareRoots()) {
+//            Math.sqrt()
+//        }
         return null;
     }
 
     public static Double[] squares(Double... values) {
+//        for(int i = 0; i < values.length; i++){
+//            double squared = values[i] * values[i];
+//        }
+//        return squared
+
+
+//        ArrayList<Double> arr = new ArrayList<Double>();
+//        for(Double value : arr) {
+//            for(int i = 0; i < squares().length; i++) {
+//            }
+//        }
         return null;
     }
 
     public static Double add(Double value1, Double value2) {
-        return null;
+        return value1 + value2;
     }
 
     public static Double subtract(Double value1, Double value2) {
-        return null;
+        return value1 - value2;
     }
 
 
     public static Double divide(Double divisor, Double dividend) {
-        return null;
+        return divisor/dividend;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -5,11 +5,14 @@ package rocks.zipcode.quiz4.fundamentals;
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+        int middle = string.length()/2;
+        return string.charAt(middle);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        int middle = str.length()/2;
+        String upper = str.substring(middle).toUpperCase();
+        return upper;
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
